3aa358e0092f04ca7b21d393404bc43e00ef4494
Kate Komleva <komlevakate99@gmail.com>
2023-08-24 21:40:48 +0300

    feat: add servlet for authors

--- src/main/java/org/example/servlet/AuthorServlet.java
+++ src/main/java/org/example/servlet/AuthorServlet.java
@@ -1,8 +1,108 @@
 package org.example.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.config.ConnectionSetting;
+import org.example.model.Author;
+import org.example.repository.AuthorRepository;
+import org.example.repository.impl.AuthorRepositoryImpl;
+import org.example.service.AuthorService;
+import org.example.service.impl.AuthorServiceImpl;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/authors")
 public class AuthorServlet extends HttpServlet {
+    private AuthorService authorService;
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void init() {
+        ConnectionSetting connection = new ConnectionSetting();
+        AuthorRepository authorRepository = new AuthorRepositoryImpl(connection);
+        authorService = new AuthorServiceImpl(authorRepository);
+        objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+
+        if (id != null) {
+            Long authorId = Long.parseLong(id);
+            Author author = authorService.findById(authorId);
+
+            if (author == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                objectMapper.writeValue(response.getWriter(), author);
+            }
+
+        } else {
+            List<Author> authors = authorService.findAll();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), authors);
+
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+            Author author = objectMapper.readValue(request.getInputStream(), Author.class);
+            objectMapper.writeValue(response.getWriter(), authorService.create(author));
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+            Author updatedAuthor = objectMapper.readValue(request.getInputStream(), Author.class);
+
+            if (updatedAuthor == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                objectMapper.writeValue(response.getWriter(), authorService.update(updatedAuthor));
+                response.setStatus(HttpServletResponse.SC_OK);
+            }
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+
+        if (id != null) {
+            Long authorId = Long.parseLong(id);
+            Author author = authorService.findById(authorId);
+
+            if (author == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                boolean deleted = authorService.delete(authorId);
+                if (deleted) {
+                    response.getWriter().println("Author deleted successfully.");
+                    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                }
+            }
+        }
+    }
 }
+
--- src/main/java/org/example/servlet/BookServlet.java
+++ src/main/java/org/example/servlet/BookServlet.java
@@ -3,10 +3,15 @@ package org.example.servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.config.ConnectionSetting;
 import org.example.model.Book;
+import org.example.model.Genre;
 import org.example.repository.BookRepository;
+import org.example.repository.GenreRepository;
 import org.example.repository.impl.BookRepositoryImpl;
+import org.example.repository.impl.GenreRepositoryImpl;
 import org.example.service.BookService;
+import org.example.service.GenreService;
 import org.example.service.impl.BookServiceImpl;
+import org.example.service.impl.GenreServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,16 +21,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(value = "/books", loadOnStartup = 1)
+@WebServlet("/books")
 public class BookServlet extends HttpServlet {
     private BookService bookService;
+    private GenreService genreService;
     private ObjectMapper objectMapper;
 
     @Override
     public void init() {
         ConnectionSetting connection = new ConnectionSetting();
         BookRepository bookRepository = new BookRepositoryImpl(connection);
+        GenreRepository genreRepository = new GenreRepositoryImpl(connection);
+
         bookService = new BookServiceImpl(bookRepository);
+        genreService = new GenreServiceImpl(genreRepository);
         objectMapper = new ObjectMapper();
     }
 
@@ -61,7 +70,7 @@ public class BookServlet extends HttpServlet {
 
         if (pathInfo == null || pathInfo.equals("/")) {
             Book book = objectMapper.readValue(request.getInputStream(), Book.class);
-            bookService.create(book);
+            objectMapper.writeValue(response.getWriter(), bookService.create(book));
             response.setStatus(HttpServletResponse.SC_CREATED);
         } else {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST);
@@ -71,6 +80,23 @@ public class BookServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pathInfo = request.getPathInfo();
+        String id1 = request.getParameter("bookId");
+        String id2 = request.getParameter("genreId");
+
+        if (id1 != null && id2 != null) {
+            Long bookId = Long.parseLong(id1);
+            Integer genreId = Integer.parseInt(id2);
+
+            Book book = bookService.findById(bookId);
+            Genre genre = genreService.findById(genreId);
+
+            if (book == null || genre == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                bookService.setBookGenre(bookId, genreId);
+                response.setStatus(HttpServletResponse.SC_OK);
+            }
+        }
 
         if (pathInfo == null || pathInfo.equals("/")) {
             Book updatedBook = objectMapper.readValue(request.getInputStream(), Book.class);
@@ -78,36 +104,30 @@ public class BookServlet extends HttpServlet {
             if (updatedBook == null) {
                 response.sendError(HttpServletResponse.SC_NOT_FOUND);
             } else {
-                bookService.update(updatedBook);
+                objectMapper.writeValue(response.getWriter(), bookService.update(updatedBook));
                 response.setStatus(HttpServletResponse.SC_OK);
             }
-
-        } else if (pathInfo.startsWith("/")) {
-            Long bookId = Long.valueOf(pathInfo.substring(1));
-            Integer genreId = Integer.valueOf(request.getParameter("genreId"));
-            bookService.setBookGenre(bookId, genreId);
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pathInfo = request.getPathInfo();
+        String id = request.getParameter("id");
 
-        if (pathInfo == null || pathInfo.equals("/")) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-        } else {
-            Long id = Long.parseLong(pathInfo.substring(1));
-            boolean deleted = bookService.delete(id);
+        if (id != null) {
+            Long bookId = Long.parseLong(id);
+            Book book = bookService.findById(bookId);
 
-            if (deleted) {
-                String jsonResponse = objectMapper.writeValueAsString("Book deleted successfully.");
-                response.getWriter().println(jsonResponse);
-                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            } else {
+            if (book == null) {
                 response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                boolean deleted = bookService.delete(bookId);
+                if (deleted) {
+                    response.getWriter().println("Book deleted successfully.");
+                    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                }
             }
         }
     }

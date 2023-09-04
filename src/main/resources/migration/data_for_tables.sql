2fdc62b45a407dc077ef917d47b7ed7680e1ba1f
Kate Komleva <komlevakate99@gmail.com>
2023-09-04 08:54:29 +0300

    feat: new change

--- src/main/resources/migration/data_for_tables.sql
+++ src/main/resources/migration/data_for_tables.sql
@@ -2,11 +2,13 @@ insert into public.authors (id, full_name, year_of_birth)
 values  (1, 'Lewis Carroll', 1832),
         (2, 'Stephen King', 1947);
 
+
 insert into public.books (id, name, year, author_id)
 values  (1, 'Alice in Wonderland', 1865, 1),
         (2, 'Through the Looking-Glass, and What Alice Found There', 1871, 1),
         (3, 'Carrie', 1974, 2),
         (5, 'The Shining', 1977, 2);
+
 
 insert into public.genres (id, type)
 values  (1, 'fantasy'),
